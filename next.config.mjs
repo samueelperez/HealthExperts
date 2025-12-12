a541a8726cb51a8788954313fa3c80c3ea99adfc
Samuel Perez <143922539+samueelperez@users.noreply.github.com>
2025-12-12 19:31:00 +0100

    dds

---
+++ next.config.mjs
@@ -0,0 +1,9 @@
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    reactCompiler: true,
+};
+
+export default nextConfig;
